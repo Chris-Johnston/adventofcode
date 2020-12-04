2cdad6d934d935a55b3eb870df2209da77c42883
Chris Johnston <chris@thejohnstons.net>
2020-12-03 19:46:35 -0800

    add boilerplate template for AoC solutions

---
+++ 2020/template/src/main.rs
@@ -0,0 +1,49 @@
+use std::fs;
+
+static INPUT_FILE: &str = "input.txt";
+
+static EXAMPLE_INPUT: &str = 
+"<input goes here>";
+
+const EXAMPLE_ANSWER_1: isize = 1;
+const EXAMPLE_ANSWER_2: isize = 2;
+
+fn main() {
+    let input = fs::read_to_string(INPUT_FILE)
+        .expect("failed to read file");
+
+    let example_solution = solution(EXAMPLE_INPUT)
+        .expect("no result");
+    
+    println!("Example Part 1 {} = {}", EXAMPLE_ANSWER_1, example_solution);
+    assert!(EXAMPLE_ANSWER_1 == example_solution);
+
+    let answer = solution(&input)
+        .expect("no result");
+    
+    println!("Answer Part 1 {}", answer);
+    // assert!(answer == 42069);
+
+    // part 2
+    let example_solution = solution_part2(EXAMPLE_INPUT)
+        .expect("no result");
+
+    println!("Example Part 2 {} = {}", EXAMPLE_ANSWER_2, example_solution);
+    assert!(EXAMPLE_ANSWER_2 == example_solution);
+
+    let answer = solution_part2(&input)
+        .expect("no result");
+    
+    println!("Answer Part 2 {}", answer);
+    // assert!(answer == 1355323200);
+}
+
+fn solution(input: &str) -> Option<isize>
+{
+    None
+}
+
+fn solution_part2(input: &str) -> Option<isize>
+{
+    None
+}
