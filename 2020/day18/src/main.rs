013c2ec81509766d9e49a74df2507857031c7216
Chris Johnston <chris@thejohnstons.net>
2020-12-23 00:04:49 -0800

    can't sleep, add 18 pt 2 solution which overwrites pt 1

--- 2020/day18/src/main.rs
+++ 2020/day18/src/main.rs
@@ -12,12 +12,12 @@ const EXAMPLE_ANSWER_2: usize = 2;
 fn main() {
 
     let test_cases = vec![
+        ("1 + 2 * 3 + 4 * 5 + 6", 231),
         ("1 + (2 * 3) + (4 * (5 + 6))", 51),
-        ("1 + 2 * 3 + 4 * 5 + 6", 71),
-        ("2 * 3 + (4 * 5)", 26),
-        ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437),
-        ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240),
-        ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632),
+        ("2 * 3 + (4 * 5)", 46),
+        ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445),
+        ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060),
+        ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340),
     ];
 
     for (case, result) in test_cases
@@ -38,24 +38,24 @@ fn main() {
     // println!("Example Part 1 {} = {}", EXAMPLE_ANSWER_1, example_solution);
     // assert!(EXAMPLE_ANSWER_1 == example_solution);
 
-    let answer = solution(&input)
-        .expect("no result");
+    // let answer = solution(&input)
+    //     .expect("no result");
     
-    println!("Answer Part 1 {}", answer);
+    // println!("Answer Part 1 {}", answer);
     // assert!(answer == 42069);
 
     // part 2
-    let example_solution = solution_part2(EXAMPLE_INPUT)
-        .expect("no result");
+    // let example_solution = solution_part2(EXAMPLE_INPUT)
+    //     .expect("no result");
 
-    println!("Example Part 2 {} = {}", EXAMPLE_ANSWER_2, example_solution);
-    assert!(EXAMPLE_ANSWER_2 == example_solution);
+    // println!("Example Part 2 {} = {}", EXAMPLE_ANSWER_2, example_solution);
+    // assert!(EXAMPLE_ANSWER_2 == example_solution);
 
     let answer = solution_part2(&input)
         .expect("no result");
     
     println!("Answer Part 2 {}", answer);
-    // assert!(answer == 1355323200);
+    assert!(answer == 360029542265462);
 }
 
 // struct TreeNode
@@ -152,21 +152,85 @@ fn eval(input: &str) -> usize
 
         while val_stack.len() > 1
         {
-            let a = val_stack.pop_front().unwrap();
-            let b = val_stack.pop_front().unwrap();
+            // oops I forgot to copy paste this for part 2. oh well.
 
-            let op = op_stack.pop_front().unwrap();
+            // for part 2, instead of going L to R here, would need to walk through to solve all +'s, then *'s
+            // let a = val_stack.pop_front().unwrap();
+            // let b = val_stack.pop_front().unwrap();
+            // let op = op_stack.pop_front().unwrap();
 
-            let result = match op
+            // let result = match op
+            // {
+            //     '+' => a + b,
+            //     '*' => a * b,
+            //     _ => 0,
+            // };
+            // println!("{} {} {} = {}", a, op, b, result);
+            // val_stack.push_front(result);
+
+            // stack where * ops are stored after iterating through the op_stack
+            let mut mul_stack = VecDeque::new();
+            let mut mul_val_stack = VecDeque::new();
+
+            // walk through the op stack to find all + operators
+            // might need to adjust how this is done so that it respects when things change
+            while op_stack.len() > 0
             {
-                '+' => a + b,
-                '*' => a * b,
-                _ => 0,
-            };
+                // this method feels kinda ugly, glad that there are only 2 levels of operator order (aside from paren)
 
-            println!("{} {} {} = {}", a, op, b, result);
+                let op = op_stack.pop_front().unwrap();
+                if op != '+'
+                {
+                    mul_stack.push_back(op);
+                    // pop off the value associated with this non-+ op
+                    let v = val_stack.pop_front().unwrap();
+                    mul_val_stack.push_back(v);
+                    continue;
+                }
 
-            val_stack.push_front(result);
+                // 3 * 2 + 1
+                // * +
+                // pop off 3 and stick on the mul stack
+                // remaining is 2 + 1
+                // stick result back on the val_stack
+                // when op_stack is empty, add the remaining val to the other stack
+
+                // get the operands
+                let a = val_stack.pop_front().unwrap();
+                let b = val_stack.pop_front().unwrap();
+                let result = a + b;
+
+                // remove b from the val stack and store the result in a
+                println!("{} + {} = {}", a, b, result);
+                
+                val_stack.push_front(result);
+            }
+
+            while val_stack.len() > 0
+            {
+                println!("mitigate");
+                let v = val_stack.pop_front().unwrap();
+                mul_val_stack.push_back(v);
+            }
+
+            println!("stacks after + pass: {:?} {:?} {:?} {:?}", mul_stack, mul_val_stack, op_stack, val_stack);
+
+            // iterate through the mul stack and compute that too
+            while mul_stack.len() > 0
+            {
+                mul_stack.pop_front();
+
+                let a = mul_val_stack.pop_front().unwrap();
+                let b = mul_val_stack.pop_front().unwrap();
+
+                let result = a * b;
+
+                println!("{} * {} = {}", a, b, result);
+                mul_val_stack.push_front(result);
+            }
+
+            // lazy
+            val_stack.push_front(mul_val_stack.pop_front().unwrap());
         };
 
         let answer = val_stack.pop_front().unwrap();
@@ -188,5 +252,8 @@ fn solution(input: &str) -> Option<usize>
 
 fn solution_part2(input: &str) -> Option<usize>
 {
-    None
+    let x = input
+        .lines()
+        .fold(0, |acc, x| acc +eval(x));
+    Some(x)
 }
