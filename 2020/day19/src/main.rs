5c178a17dd2a914847346f836d93b349407add9c
Chris Johnston <chris@thejohnstons.net>
2020-12-30 13:44:03 -0800

    wip day 19

--- 2020/day19/src/main.rs
+++ 2020/day19/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::collections::VecDeque;
 
 static INPUT_FILE: &str = "input.txt";
@@ -25,17 +26,20 @@ fn main() {
     let input = fs::read_to_string(INPUT_FILE)
         .expect("failed to read file");
 
-    let example_solution = solution(EXAMPLE_INPUT)
-        .expect("no result");
+    // let example_solution = solution(EXAMPLE_INPUT)
+    //     .expect("no result");
     
-    println!("Example Part 1 {} = {}", EXAMPLE_ANSWER_1, example_solution);
-    assert!(EXAMPLE_ANSWER_1 == example_solution);
+    //println!("Example Part 1 {} = {}", EXAMPLE_ANSWER_1, example_solution);
+    // assert!(EXAMPLE_ANSWER_1 == example_solution);
 
     let answer = solution(&input)
         .expect("no result");
     
     println!("Answer Part 1 {}", answer);
     // assert!(answer == 42069);
+
+    // part 1 is 213
+    // part 2 is more than 213
 
     // part 2
     let example_solution = solution_part2(EXAMPLE_INPUT)
@@ -247,14 +251,14 @@ fn build_expression_tree(literal_rules: &HashMap<usize, char>, nested_rules: &Ha
 // expands into 3 strings - a and ba and aaa
 
 fn generate_expressions(literal_rules: &HashMap<usize, char>, nested_rules: &HashMap<usize, Vec<Vec<usize>>>)
--> std::collections::HashMap<usize, std::vec::Vec<std::string::String>> {
+-> std::collections::HashMap<usize, HashSet<std::string::String>> {
     let mut treeheads = HashMap::new();
     for (&key, &value) in literal_rules
     {
-        let mut literal = Vec::new();
+        let mut literal = HashSet::new();
         // literal.push(value.to_string().as_str());
         let v = String::from(value);
-        literal.push(v);
+        literal.insert(v);
         treeheads.insert(key, literal);
     }
 
@@ -265,6 +269,9 @@ fn generate_expressions(literal_rules: &HashMap<usize, char>, nested_rules: &Has
     let mut dependency_stack = VecDeque::new();
 
     dependency_stack.push_front(0);
+    dependency_stack.push_front(31);
+    dependency_stack.push_front(42);
+    dependency_stack.push_front(8);
 
     while dependency_stack.len() > 0
     {
@@ -281,7 +288,68 @@ fn generate_expressions(literal_rules: &HashMap<usize, char>, nested_rules: &Has
             continue;
         }
 
-        let mut current = Vec::new();
+        let mut current = HashSet::new();
+
+        if key == 11
+        {
+            println!("RULE 11");
+            // append pairs of 42's and 31's
+            for pairs in 1..10
+            {
+                println!("pairs: {}", pairs);
+                let fourtytwo = &treeheads[&42];
+                let thirtyone = &treeheads[&31];
+
+                    for append42 in fourtytwo
+                    {
+                        for append31 in thirtyone
+                        {
+                            let mut si = String::new();
+                            for _ in 0..pairs
+                    {
+                        
+                            si.push_str(append42);
+                    }
+                    for _ in 0..pairs
+                    {
+                        
+                            si.push_str(append31);
+                    }
+
+                    // println!("pushing {}", si);
+
+                    if si.len() < 100
+                    {
+                        break;
+                    }
+                    else{
+                        current.insert(si);
+                    }
+                        }
+                    }
+            }
+
+            treeheads.insert(key, current);
+            continue;
+        }
+        // else if key == 8
+        // {
+        //     let mut current = HashSet::new();
+        //     let fourtytwo = &treeheads[&42];
+        //     for _ 0..100
+        //     {
+        //         let mut si = String::new();
+
+        //         for a in fourtytwo
+        //         {
+        //             si.push_str(a);
+        //         }
+        //         si.push_str(fourtytwo);
+        //     }
+
+        //     treeheads.insert(key, current);
+        //     continue;
+        // }
         for sub_rule in current_rule
         {
             println!("\tsub rule {:?}", sub_rule);
@@ -305,8 +373,29 @@ fn generate_expressions(literal_rules: &HashMap<usize, char>, nested_rules: &Has
                         {
                             let mut si = String::from(si);
                             si.push_str(append.as_str());
-                            println!("si: {}", si);
+                            // println!("si: {}", si);
                             combinations.push(si.clone());
+
+                            // println!("si len {}", si.len());
+
+                            if si.len() > 100
+                            {
+                                break;
+                            }
+
+                            // special cases for part 2
+                            // this is such a hack lol
+                            // if key == 8
+                            // {
+                            //     // this part fails because there are different cobinations for 42
+                            //     // println!("special case for 8");
+                            //     for x in 0..100
+                            //     {
+                            //         si.push_str(append.as_str());
+                            //         println!("{}", si);
+                            //         combinations.push(si.clone());
+                            //     }
+                            // }
                         }
 
                         if first_pass
@@ -315,8 +404,7 @@ fn generate_expressions(literal_rules: &HashMap<usize, char>, nested_rules: &Has
                             combinations.push(append.clone());
                         }
                     }
-
-                    println!("\tpart {:?} combinations {:?}", part, combinations);
+                    println!("\tpart {:?} combinations {:?}", part, combinations.len());
                     s = combinations;
                 }
                 else
@@ -330,12 +418,18 @@ fn generate_expressions(literal_rules: &HashMap<usize, char>, nested_rules: &Has
                 }
             }
 
-            current.append(&mut s);
+            // current.append(&mut s);
+            for x in s
+            {
+                if x.len() < 100
+                {
+                current.insert(x);}
+            }
         }
 
         if all_defined
         {
-            println!("Set tree {} == {:?}", key, current);
+            println!("Set tree {} == {:?}", key, current.len());
             treeheads.insert(key, current);
         }
         else
@@ -372,14 +466,20 @@ fn solution(input: &str) -> Option<isize>
     for expression in expressions.lines()
     {
         let possible = g.get(&0).unwrap();
-        for x in possible
+
+        if possible.contains(expression)
         {
-            if x == expression
-            {
-                count += 1;
-                break;
-            }
+            count += 1;
         }
+
+        // for x in possible
+        // {
+        //     if x == expression
+        //     {
+        //         count += 1;
+        //         break;
+        //     }
+        // }
     }
 
     Some(count)
